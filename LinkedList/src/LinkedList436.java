acde1cd070aeb97aada4ca322b1b1b090bae1cb4
Josue Morales <molinajosue92@hotmail.com>
2023-02-22 14:46:18 -0500

    Add Files

---
+++ LinkedList/src/LinkedList436.java
@@ -0,0 +1,59 @@
+
+public class LinkedList436 {
+	private int counter;
+	private Node head;
+ 
+	// appends the specified element to the end of this list.
+	public void add(Object data) {
+ 
+		// PLEASE ADD YOUR CODE HERE.
+
+		
+	}
+	
+	// inserts the specified element at the specified position in this list
+	public void add(Object data, int index) {
+		 
+		// PLEASE ADD YOUR CODE HERE.
+
+		
+	}
+	
+	// returns the element at the specified position in this list.
+	public Object get(int index)
+	{
+		 
+		// PLEASE ADD YOUR CODE HERE.
+
+	
+	}
+ 
+	// removes the element at the specified position in this list.
+	public boolean remove(int index) {
+		 
+		// PLEASE ADD YOUR CODE HERE.
+
+	
+	}
+ 
+ 
+	// returns the String content of all the nodes in this list. 
+	public String toString() {
+		 
+		// PLEASE ADD YOUR CODE HERE.
+
+	
+	}
+ 
+	// returns the number of elements in this list.
+	public int size() {
+		return counter;
+	}
+	
+	
+	private class Node {
+
+		// PLEASE FINISH THIS INNER CLASS.
+ 
+	}
+}
