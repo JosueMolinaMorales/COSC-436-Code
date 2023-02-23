ce41dde745bbe2cdf8b43e70408c161ca46b8a1a
Josue Morales <molinajosue92@hotmail.com>
2023-02-23 18:02:32 -0500

    add impl for node inner class

--- LinkedList/src/LinkedList436.java
+++ LinkedList/src/LinkedList436.java
@@ -5,44 +5,87 @@ public class LinkedList436 {
  
 	// appends the specified element to the end of this list.
 	public void add(Object data) {
- 
-		// PLEASE ADD YOUR CODE HERE.
-
-		
+		if (head == null) {
+			head = new Node(data);
+			counter++;
+			return;
+		}
+		Node temp = new Node(data);
+		Node current = head;
+		while (current.getNext() != null) {
+			current = current.getNext();
+		}
+		current.setNext(temp);
+		counter++;
 	}
 	
 	// inserts the specified element at the specified position in this list
 	public void add(Object data, int index) {
-		 
-		// PLEASE ADD YOUR CODE HERE.
-
-		
+		int i = 0;
+		Node temp = new Node(data);
+		Node current = head;
+		while (i < index - 1) {
+			if (current == null) {
+				throw new IndexOutOfBoundsException();
+			}
+			current = current.getNext();
+			i++;
+		}
+		temp.setNext(current.getNext());
+		current.setNext(temp);
+		counter++;
 	}
 	
 	// returns the element at the specified position in this list.
-	public Object get(int index)
-	{
-		 
-		// PLEASE ADD YOUR CODE HERE.
-
-	
+	public Object get(int index){
+		if (index < 0 || index >= counter) {
+			throw new IndexOutOfBoundsException();
+		}
+		int i = 0;
+		Node current = head;
+		while (i < index) {
+			if (current == null) {
+				throw new IndexOutOfBoundsException();
+			}
+			current = current.getNext();
+			i++;
+		}
+		return current.getData();
 	}
  
 	// removes the element at the specified position in this list.
 	public boolean remove(int index) {
-		 
-		// PLEASE ADD YOUR CODE HERE.
-
-	
+		if (index < 0 || index >= counter) {
+			throw new IndexOutOfBoundsException();
+		}
+		int i = 0;
+		Node current = head;
+		while (i < index - 1) {
+			if (current == null) {
+				throw new IndexOutOfBoundsException();
+			}
+			current = current.getNext();
+			i++;
+		}
+		current.setNext(current.getNext().getNext());
+		counter--;
+		return true;
 	}
  
  
 	// returns the String content of all the nodes in this list. 
 	public String toString() {
-		 
-		// PLEASE ADD YOUR CODE HERE.
-
-	
+		String output = "[";
+		if (head == null) {
+			return "[]";
+		}
+		Node current = head;
+		while (current != null) {
+			output += current.getData().toString() + ",";
+			current = current.getNext();
+		}
+		output = output.substring(0, output.length() - 1) + "]";
+		return output;
 	}
  
 	// returns the number of elements in this list.
@@ -52,8 +95,35 @@ public class LinkedList436 {
 	
 	
 	private class Node {
+		// reference to the next node in the chain, or null if there isn't one.
+		Node next;
 
-		// PLEASE FINISH THIS INNER CLASS.
- 
+		// data carried by this node. could be of any type you need.
+		Object data;
+
+		// Node constructor
+		public Node(Object dataValue) {
+			next = null;
+			data = dataValue;
+		}
+
+		// these methods should be self-explanatory
+		public Object getData() {
+			return data;
+		}
+
+		@SuppressWarnings("unused")
+		public void setData(Object dataValue) {
+			data = dataValue;
+		}
+			
+		// return the next node of the current node
+		public Node getNext() {
+			return next;
+		}
+			
+		public void setNext(Node nextValue) {
+			next = nextValue;
+		}
 	}
 }
